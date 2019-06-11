5a392147640dfe769406d8f9862adb7bf6a46100
pedrocampeloa <45722270+pedrocampeloa@users.noreply.github.com>
2019-06-11 12:44:59 -0300

    Create open files.py

---
+++ open files.py
@@ -0,0 +1,66 @@
+    
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  9 20:12:05 2019
+@author: pedrocampelo
+"""
+
+#bibliotecas importadas
+import pandas as pd
+
+
+#definir o diretório
+def input_wd(work_path):
+  """
+  Antes de abrir o arquivo é necessário definir o local de trablalho. 
+  Basta colar o o local de trabalho quando chamar a função e ela ajusta as barras (que sao ao contrario no python)
+  e define qual o local de trabalho pela biblioteca os
+  Verificar atributos em:
+  
+  """
+  
+  return 
+
+
+#abrir arquivos txt
+def open_txt():
+  """
+  Esta função
+  Verificar atributos em:  
+  """
+  
+  return 
+  
+  
+#abrir arquivos csv
+def open_csv():
+  """
+  Esta função
+  Verificar atributos em:  
+  
+  """
+  
+  return 
+
+#abrir arquivos xlsx/xsl
+def open_excel():
+  """
+  Esta função
+  Verificar atributos em:  
+  
+  """
+  
+  return 
+
+#abrir arquivos xslx com senha
+def open_excel_passwd():
+  """
+  Esta função
+  Verificar atributos em:  
+  
+  """
+  
+  return 
+
+
